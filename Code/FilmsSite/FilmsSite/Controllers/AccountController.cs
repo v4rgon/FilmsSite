e7c92ff7a32c3347d89af6eacb1ad28fc7d6d59a
rokez98 <vrokez@gmail.com>
2018-10-27 16:53:44 +0300

    Controllers created

---
+++ Code/FilmsSite/FilmsSite/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace FilmsSite.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+    }
+}
---
+++ Code/FilmsSite/FilmsSite/Controllers/FilmsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmsSite.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class FilmsController : Controller
+    {
+    }
+}
