9941be87b6eb247c283a5fa611ba30b74fc48460
rokez98 <vrokez@gmail.com>
2018-10-27 20:19:06 +0300

    Token Controller added

--- Code/FilmsSite/FilmsSite/Controllers/TokenController.cs
+++ Code/FilmsSite/FilmsSite/Controllers/TokenController.cs
@@ -1,9 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using FilmsSite.BLL.Interfaces;
+using FilmsSite.BLL.Models;
+using FilmsSite.WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace FilmsSite.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     public class TokenController : Controller
     {
+        private readonly ITokenService _tokenService;
+        private readonly IUserService _userService;
+
+        public TokenController(ITokenService tokenService, IUserService userService)
+        {
+            _tokenService = tokenService;
+            _userService = userService;
+        }
+
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh([FromBody] RefreshTokenModel model)
+        {
+            var user = await _userService.GetByUsernameAsync(model.UserName);
+            if (user == null) return BadRequest("User doesn't exist!");
+
+            if (user.RefreshToken != model.RefreshToken || user.RefreshToken == null) return BadRequest("Wrong refresh token!");
+
+            var newJwtToken = await _tokenService.GenerateJwtToken(user.UserName, user.Id);
+
+            return Ok(Mapper.Map<TokenDTO, Token>(newJwtToken));
+        }
+
+        [HttpPost("revoke"), Authorize]
+        public async Task<IActionResult> Revoke()
+        {
+            var username = User.Identity.Name;
+
+            var user = await _userService.GetByUsernameAsync(username);
+            if (user == null) return BadRequest("User doesn't exist!");
+
+            await _userService.UpdateUsersRefreshTokenAsync(username, null);
+
+            return Ok();
+        }
     }
 }
---
+++ Code/FilmsSite/FilmsSite/Models/RefreshTokenModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace FilmsSite.WebAPI.Models
+{
+    public class RefreshTokenModel
+    {
+        [JsonProperty("userName")]
+        public string UserName { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+    }
+}
